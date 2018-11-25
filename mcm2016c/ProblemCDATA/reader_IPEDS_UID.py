c729ba664176b71067ee00b424d757effe604abd
Hanyuu <HanyuuFurude@outlook.com>
2018-11-25 12:41:59 +0800

    201811251241

--- mcm2016c/ProblemCDATA/reader_IPEDS_UID.py
+++ mcm2016c/ProblemCDATA/reader_IPEDS_UID.py
@@ -6,14 +6,16 @@ class Reader:
         print('[tips]请将数据文件'+fileName+"放在目录"+filePath+"下")
         print('[info] Start to load')
         import xlrd
-        data = xlrd.open_workbook(filePath+'\\'+fileName)
-        table = data.sheets()[0]
-        for i in range(table.nrows):
-            print(table.row_values(i))
+        self.data = xlrd.open_workbook(filePath+'\\'+fileName)
+        self.table = self.data.sheets()[0]
+
+    def print(self):
+        for i in range(self.table.nrows):
+            print(self.table.row_values(i))
 
 
 if __name__ == '__main__':
     import os
     print('[path] '+os.path.dirname(os.path.abspath(__file__)))
     reader = Reader()
-    reader.__init__
+    reader.print()
