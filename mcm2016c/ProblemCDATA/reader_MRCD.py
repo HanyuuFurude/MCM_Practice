032166ae82c625b329b0c0d1f52ae426b6dc96c6
Hanyuu <HanyuuFurude@outlook.com>
2018-11-25 12:41:59 +0800

    201811251241

---
+++ mcm2016c/ProblemCDATA/reader_MRCD.py
@@ -0,0 +1,21 @@
+class Reader:
+
+    def __init__(self):
+        fileName = '''MRCD.xlsx'''
+        filePath = os.path.dirname(os.path.abspath(__file__))
+        print('[tips]请将数据文件'+fileName+"放在目录"+filePath+"下")
+        print('[info] Start to load')
+        import xlrd
+        self.data = xlrd.open_workbook(filePath+'\\'+fileName)
+        self.table = self.data.sheets()[0]
+
+    def print(self):
+        for i in range(self.table.nrows):
+            print(self.table.row_values(i))
+
+
+if __name__ == '__main__':
+    import os
+    print('[path] '+os.path.dirname(os.path.abspath(__file__)))
+    reader = Reader()
+    reader.print()
