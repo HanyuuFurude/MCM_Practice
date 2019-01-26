344ebc1bc80076d9e5427e1c975f295635768ff7
Hanyuu <HanyuuFurude@outlook.com>
2019-01-26 10:13:23 +0800

    201901261013

--- 2019/D/roomReader.py
+++ 2019/D/roomReader.py
@@ -1,4 +1,22 @@
 import reader
+print(type(type('hanyuu')))
 
-readIn = reader.Reader('data.xlsx')
-mat = readIn.getMartix()
+
+class roomReader:
+    def __init__(self, fileName='floor.slxs'):
+        readIn = reader.Reader(fileName)
+        mat = readIn.getMartix()
+        self.dic = {}
+        for x in mat[0]:
+            self.dic[str(int(x[1]))] = int(x[0])
+
+    def query(self, s):
+        if isinstance(s, str) is False:
+            raise TypeError('str required,[Hanyuu]')
+        else:
+            return self.dic[s]
+
+
+if __name__ == '__main__':
+    s = roomReader('floor.xlsx')
+    print(s.query('201'))
