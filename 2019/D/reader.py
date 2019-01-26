95c779105a91ed080ce6cf7043aab5707d0083a5
Hanyuu <HanyuuFurude@outlook.com>
2019-01-26 09:43:34 +0800

    201901260943

---
+++ 2019/D/reader.py
@@ -0,0 +1,45 @@
+class Reader:
+
+    def __init__(self, fileName, filePath=None):
+        # self.fileName = '''data.xlsx'''
+        import os
+        self.fileName = fileName
+        if filePath == None:
+            self.filePath = os.path.dirname(os.path.abspath(__file__))
+        print('[tips]请将数据文件'+self.fileName+"放在目录"+self.filePath+"下")
+        print('[info] Start to load')
+        import xlrd
+        self.data = xlrd.open_workbook(self.filePath+'\\'+self.fileName)
+        self.sheets = self.data.sheets()
+
+    def print(self):
+        for i in range(len(self.sheets)):
+            print('\n[table' + str(i)+']\n'+self.sheets[i].name)
+            for j in range(self.sheets[i].nrows):
+                print(self.sheets[i].row_values(j))
+
+    def getMartix(self):
+        matrix = []
+        for i in range(len(self.sheets)):
+            subMat = []
+            for j in range(self.sheets[i].nrows):
+                subMat.append(self.sheets[i].row_values(j))
+            matrix.append(subMat)
+        return matrix
+
+    def fileInfo(self):
+        print('['+self.fileName+']'+' has '+str(len(mat))+' table(s)')
+        for i in range(len(self.sheets)):
+            print('\t[table '+str(i)+'] '+str(self.sheets[i].ncols) +
+                  ','+str(self.sheets[i].nrows))
+
+
+if __name__ == '__main__':
+    import os
+    print('[path] '+os.path.dirname(os.path.abspath(__file__)))
+    reader = Reader('''data.xlsx''')
+    # reader.print()
+    mat = reader.getMartix()
+    reader.print()
+    reader.fileInfo()
+    print(reader.getMartix())
---
+++ 2019/D/roomReader.py
@@ -0,0 +1,4 @@
+import reader
+
+readIn = reader.Reader('data.xlsx')
+mat = readIn.getMartix()
