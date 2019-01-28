5d321849f79ba4a010a34c9757071428f5d5bebf
Hanyuu <HanyuuFurude@outlook.com>
2019-01-28 11:43:37 +0800

    201901281143

--- 2019/D/code/people.py
+++ 2019/D/code/people.py
@@ -12,7 +12,7 @@ from fs import fs
 class group:
     def __init__(self, uuid, position, count, port=None, por=0):
         self.gid = uuid
-        self.gSpeed = 0.6
+        self.gSpeed = 1.6
         self.gPosition = position
         self.gPositionOnRoad = por  # 若在路径上，记录当前行进距离
         self.gPositionOnRoadTo = port  # 若在路径上，记录目标容器
@@ -34,7 +34,7 @@ class groupList:
                 self.newNodeList[fs(x[i])] = []
         self.matrix = Reader('floor.xlsx').getMartix()[0]
         for x in self.matrix:
-            self.nodeList[fs(x[1])].append(group(0, fs(x[1]), int(x[3])))
+            self.nodeList[fs(x[1])].append(group(0, fs(x[1]), int(x[3]))
                                                 #uuid 房间号 人数
     def load(self, ID):
         sumUp = 0
